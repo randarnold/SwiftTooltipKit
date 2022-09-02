0a536d444dd64e01a2f5327cd3dd0cf97a12b8ba
Rand Arnold <14sailor@gmail.com>
2022-09-02 14:10:53 -0700

    Anchor the tooltip to the presenting view, so the tooltip will move if the presenting view moves.

--- Sources/SwiftTooltipKit/Extensions.swift
+++ Sources/SwiftTooltipKit/Extensions.swift
@@ -66,20 +66,15 @@ extension UIView {
         
         return activeTooltips.compactMap { $0 as? Tooltip }.contains(where: { $0.presentingView == self })
     }
-    
-    /// Presents a tooltip to the calling view with a given view, orientation and configuration.
-    ///
-    /// - Parameter view    :The view that will be displayed within the tooltip
-    /// - Parameter orientation: The placement of the tooltip in relation to the presenting view
-    /// - Parameter configuration: The configuration allowing to customize the tooltip
-    public func tooltip(_ view: UIView, orientation: Tooltip.Orientation, configuration: Tooltip.ToolTipConfiguration = Tooltip.ToolTipConfiguration()) {
-        guard !hasActiveTooltip else { return }
 
-        let toolTip = Tooltip(view: view, presentingView: self, orientation: orientation, configuration: configuration)
-        
+    /// Add the tooltip as a subview
+    /// - Parameter tooltip: The tooltip that will added as a subView
+
+    fileprivate func add(tooltip: Tooltip) {
+
         if #available(iOS 13, *) {
             if let window = window {
-                window.addSubview(toolTip)
+                window.addSubview(tooltip)
             } else if UIApplication.shared.supportsMultipleScenes {
                 os_log("Support for multiple scenes not implemented", type: .error)
             } else {
@@ -89,21 +84,99 @@ extension UIView {
                         return
                     }
                     if let window = windowScene.windows.first {
-                        window.addSubview(toolTip)
+                        window.addSubview(tooltip)
                     }
                 }
             }
         } else {
             if let window = window {
-                window.addSubview(toolTip)
+                window.addSubview(tooltip)
             } else if let window = UIApplication.shared.keyWindow {
-                window.addSubview(toolTip)
+                window.addSubview(tooltip)
             } else {
                 assertionFailure("No window available, did you set before viewDidAppear()?")
             }
         }
+    }
 
-        toolTip.present()
+    /// Anchor the tooltip
+    /// - Parameter tooltip: The tooltip that will be anchored
+    /// - Parameter orientation: The orientation of the tool tooltip
+    /// - Parameter configuration: The configuration for the toolTip
+
+    fileprivate func anchor(tooltip: Tooltip, orientation: Tooltip.Orientation, configuration: Tooltip.ToolTipConfiguration) {
+
+        // First anchor the tooltip within the safeAreaLayoutGuide
+        if let safeAreaLayoutGuide = window?.safeAreaLayoutGuide {
+            NSLayoutConstraint.activate([
+                tooltip.topAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.topAnchor, constant: Tooltip.margin),
+                tooltip.bottomAnchor.constraint(lessThanOrEqualTo: safeAreaLayoutGuide.bottomAnchor, constant: -Tooltip.margin),
+                tooltip.leadingAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.leadingAnchor, constant: Tooltip.margin),
+                tooltip.trailingAnchor.constraint(lessThanOrEqualTo: safeAreaLayoutGuide.trailingAnchor, constant: -Tooltip.margin),
+            ])
+        }
+
+        // Then anchor the tooltip to the view (self)
+        switch orientation {
+            case .leading, .left:
+                let centerYAnchor = tooltip.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+                centerYAnchor.priority = .defaultHigh
+                NSLayoutConstraint.activate([
+                    tooltip.trailingAnchor.constraint(equalTo: self.leadingAnchor, constant: configuration.offset),
+                    centerYAnchor
+                ])
+            case .trailing, .right:
+                let centerYAnchor = tooltip.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+                centerYAnchor.priority = .defaultHigh
+                NSLayoutConstraint.activate([
+                    tooltip.leadingAnchor.constraint(equalTo: self.trailingAnchor, constant: configuration.offset),
+                    centerYAnchor
+                ])
+            case .top:
+                let centerXAnchor = tooltip.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+                centerXAnchor.priority = .defaultHigh
+                NSLayoutConstraint.activate([
+                    tooltip.bottomAnchor.constraint(equalTo: self.topAnchor, constant: configuration.offset),
+                    centerXAnchor
+                ])
+            case .bottom:
+                let centerXAnchor = tooltip.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+                centerXAnchor.priority = .fittingSizeLevel
+                NSLayoutConstraint.activate([
+                    tooltip.topAnchor.constraint(equalTo: self.bottomAnchor, constant: configuration.offset),
+                    centerXAnchor
+                ])
+        }
+    }
+
+    public var tooltip: Tooltip? {
+
+        for window in UIApplication.shared.windows {
+            let activeTooltips = window.subviews.filter({ $0 is Tooltip })
+
+            for case let tooltip as Tooltip in activeTooltips {
+                if tooltip.presentingView == self {
+                    return tooltip
+                }
+            }
+        }
+
+        return nil
+    }
+    
+    /// Presents a tooltip to the calling view with a given view, orientation and configuration.
+    ///
+    /// - Parameter view: The view that will be displayed within the tooltip
+    /// - Parameter orientation: The placement of the tooltip in relation to the presenting view
+    /// - Parameter configuration: The configuration allowing to customize the tooltip
+    public func tooltip(_ view: UIView, orientation: Tooltip.Orientation, configuration: Tooltip.ToolTipConfiguration = Tooltip.ToolTipConfiguration()) {
+        guard !hasActiveTooltip else { return }
+
+        let tooltip = Tooltip(view: view, presentingView: self, orientation: orientation, configuration: configuration)
+        
+        add(tooltip: tooltip)
+        anchor(tooltip: tooltip, orientation: orientation, configuration: configuration)
+        tooltip.present()
     }
     
     /// Presents a tooltip to the calling view with a given text, orientation and configuration.
@@ -121,35 +194,11 @@ extension UIView {
         label.text = text
         label.preferredMaxLayoutWidth = configuration.labelConfiguration.preferredMaxLayoutWidth
         
-        let toolTip = Tooltip(view: label, presentingView: self, orientation: orientation, configuration: configuration)
+        let tooltip = Tooltip(view: label, presentingView: self, orientation: orientation, configuration: configuration)
 
-        if #available(iOS 13, *) {
-            if let window = window {
-                window.addSubview(toolTip)
-            } else if UIApplication.shared.supportsMultipleScenes {
-                os_log("Support for multiple scenes not implemented", type: .error)
-            } else {
-                if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
-                    guard windowScene.windows.count == 1 else {
-                        os_log("Support for multiple windows not implemented", type: .error)
-                        return
-                    }
-                    if let window = windowScene.windows.first {
-                        window.addSubview(toolTip)
-                    }
-                }
-            }
-        } else {
-            if let window = window {
-                window.addSubview(toolTip)
-            } else if let window = UIApplication.shared.keyWindow {
-                window.addSubview(toolTip)
-            } else {
-                assertionFailure("No window available, did you set before viewDidAppear()?")
-            }
-        }
-
-        toolTip.present()
+        add(tooltip: tooltip)
+        anchor(tooltip: tooltip, orientation: orientation, configuration: configuration)
+        tooltip.present()
     }
     
     /// Presents a tooltip to the calling view with a given view, orientation and configuration closure.
